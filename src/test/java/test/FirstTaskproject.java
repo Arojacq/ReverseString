fe0fe392c752fb8e776922e84995f32ffe9c217b
AroJacq <arojoseph1987@gmail.com>
2019-08-22 20:21:56 +0530

    Reverse String

---
+++ src/test/java/test/FirstTaskproject.java
@@ -0,0 +1,20 @@
+package test;
+
+
+public class FirstTaskproject {
+
+public static void main(String[] args) {
+	
+		String a ="Greens Technology";
+		String reverse ="";
+		for (int i =a.length()-1; i>=0; i--) {
+			char c = a.charAt(i);
+					reverse =reverse+c;
+	
+}
+		System.out.println(reverse);
+	
+	}		
+
+	
+}
